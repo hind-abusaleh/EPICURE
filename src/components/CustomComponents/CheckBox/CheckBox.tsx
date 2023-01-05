3f72ffe6118e1709ad94f1f531028cd0be72bea4
hind-abusaleh <73392916+hind-abusaleh@users.noreply.github.com>
2023-01-05 23:01:34 +0200

    Commit message

--- src/components/CustomComponents/CheckBox/CheckBox.tsx
+++ src/components/CustomComponents/CheckBox/CheckBox.tsx
@@ -1,26 +1,51 @@
-import * as React from 'react';
+import  React, { useState } from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import { orange } from '@mui/material/colors';
+import Box from '@mui/material/Box';
+import { info } from 'console';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 export default function CheckBox(props:{ options: string[]}) {
-    const options = props.options;
+  const options = props.options;
+  const theme = createTheme({
+    typography: {
+      fontSize: 16,
+      fontFamily: 'Helvetica Neue',
+    },
+    
+  });
+
+  const [checked, setChecked] = React.useState([false, false]);
+
+  const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked([event.target.checked, checked[1]]);
+  };
+
+  const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked([checked[0], event.target.checked]);
+  };
   return (
-    <FormControl component="fieldset">
-      <FormGroup aria-label="position" >
-        <FormControlLabel
-          value="top"
-          control={<Checkbox />}
-          label={options[0]}
-        />
-        <FormControlLabel
-          value="start"
-          control={<Checkbox />}
-          label={options[1]}
-        />
-      </FormGroup>
-    </FormControl>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex',
+       flexDirection: 'column' ,
+       gap:'1.5rem' ,
+       letterSpacing: 1.97,
+       fontWeight: 200 ,
+       lineHeight: 18,
+             }}>
+      <FormControlLabel 
+        label={options[0]}
+        control={<Checkbox sx={{ }}
+           checked={checked[0]} onChange={handleChange1} color={"warning"}/>}
+      />
+      <FormControlLabel
+        label={options[1]}
+        control={<Checkbox checked={checked[1]} onChange={handleChange2} color={"warning"}/>}
+      />
+    </Box>
+    </ThemeProvider>
   );
 }
